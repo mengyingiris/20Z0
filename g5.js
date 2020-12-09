b5b3aac3e47b6128e0df8de265b28c2d0d1e2aa8
mengyingiris <60266182+mengyingiris@users.noreply.github.com>
2020-12-08 20:53:57 -0600

    Add files via upload

---
+++ g5.js
@@ -0,0 +1,23 @@
+//here is our array of aphorisms
+
+
+
+//here is our array of aphorisms
+
+
+window.onload = choosePic;
+
+
+var myPix1 = new Array('assets2/8.JPG', 'assets2/3.JPG', 'assets2/2.JPG');
+var myPix2 = new Array('assets2/1.JPG', 'assets2/4.JPG', 'assets2/5.JPG', 'assets2/7.JPG');
+
+
+   function choosePic() {
+        var randomNum1 = Math.floor(Math.random() * myPix1.length);
+        document.getElementById("myPicture1").src = myPix1[randomNum1];
+          setTimeout("choosePic()", 2000);
+
+          var randomNum2 = Math.floor(Math.random() * myPix2.length);
+          document.getElementById("myPicture2").src = myPix2[randomNum2];
+
+      }
