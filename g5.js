642b1cf81392a2d639cfeaf0ab0d7c96fad1f955
mengyingiris <60266182+mengyingiris@users.noreply.github.com>
2020-12-15 11:40:00 -0600

    Update g5.js

--- g5.js
+++ g5.js
@@ -7,9 +7,8 @@
 
 window.onload = choosePic;
 
-
-var myPix1 = new Array('assets2/8.JPG', 'assets2/3.JPG', 'assets2/2.JPG');
-var myPix2 = new Array('assets2/1.JPG', 'assets2/4.JPG', 'assets2/5.JPG', 'assets2/7.JPG');
+var myPix1 = new Array('assets2/8.JPG', 'assets2/3.JPG', 'assets2/2.JPG','assets2/9.jpg', 'assets2/10.jpg',);
+var myPix2 = new Array('assets2/1.JPG', 'assets2/4.JPG', 'assets2/7.JPG','assets2/9.jpg','assets2/11.jpg');
 
 
    function choosePic() {
